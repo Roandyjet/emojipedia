663e22aed06df04fa4d05ba1bd35d89bf84d101f
Rova <roandyjet@gmail.com>
2022-11-13 11:48:07 +0300

    Second commit with arrow function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import Entry from "./components/Entry";
 import emojipedia from "./emojipedia";
 
-function createEntry(emoji) {
-  return (
-    <Entry
-      key={emoji.id}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
-    />
-  );
-}
+// function createEntry(emoji) {
+//   return (
+//     <Entry
+//       key={emoji.id}
+//       emoji={emoji.emoji}
+//       name={emoji.name}
+//       meaning={emoji.meaning.substring(0, 100)}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -19,8 +19,19 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      {/* <dl className="dictionary">{emojipedia.map(createEntry)}</dl> */}
+      <dl className="dictionary">
+        {emojipedia.map((emoji) => {
+          return (
+            <Entry
+              key={emoji.id}
+              emoji={emoji.emoji}
+              name={emoji.name}
+              meaning={emoji.meaning.substring(0, 100)}
+            />
+          );
+        })}
+      </dl>
     </div>
   );
 }
